c6d517909e11eeae1ced293066ebdf53cf530bd6
NarenNades <344941885@tdsb.ca>
2026-06-01 15:39:17 +0000

    notDone!

--- HelloFX.java
+++ HelloFX.java
@@ -1,19 +1,45 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.animation.AnimationTimer;
 
 public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Rectangle player = new Rectangle(75, 75);
+        Pane game = new Pane();
+        game.getChildren().add(player);
+        Scene scene = new Scene(game, 640, 500);
         stage.setScene(scene);
         stage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+            scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.S) {
+                player.setY(player.getY() + 10);
+            }
+            if (event.getCode() == KeyCode.A) {
+                player.setX(player.getX() - 10);
+            }
+            if (event.getCode() == KeyCode.W) {
+                player.setY(player.getY() - 10);
+            }
+            if (event.getCode() == KeyCode.D) {
+                player.setX(player.getX() + 10);
+            }
+        });
+    }
+        };
+        timer.start();
     }
 
     public static void main(String[] args) {
